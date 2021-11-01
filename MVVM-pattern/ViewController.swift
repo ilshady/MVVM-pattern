b5110ffd22399e55152d86c127f66733853cfcd1
ilshady <ilshat.khaitakhu@gmail.com>
2021-11-01 20:43:21 +0600

    first commit

---
+++ MVVM-pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-pattern
+//
+//  Created by Ilshat Khairakhun on 01.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
